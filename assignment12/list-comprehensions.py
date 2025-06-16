b4ca4f2197bc53e3b2ebeab39f8fb773944ea306
Boris Khenzykhenov <123456789+odin@users.noreply.github.com>
2025-06-16 14:06:27 -0400

    fix

--- assignment12/list-comprehensions.py
+++ assignment12/list-comprehensions.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 df = pd.read_csv('../csv/employees.csv')
 
-names_list = [f"{row["first_name"]} {row["last_name"]}" for _, row in df.iterrows()]
+names_list = [row["first_name"] + " "+ row["last_name"] for _, row in df.iterrows()]
 print(names_list)
 
 names_e = [name for name in names_list if 'e' in name.lower()]
